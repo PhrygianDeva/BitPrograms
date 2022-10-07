433574865fd7ffe037b894af128ff64430ebb35e
Debmalya Chakraborty <debmalyachakraborty27@gmail.com>
2022-10-07 19:54:00 +0530

    shift commit

---
+++ shift.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 15;
+    int right_shift = 15 >> 1;
+    int left_shift = 15 << 1;
+    cout<<"After 1 right shifting the value is "<<right_shift<<endl;
+    cout<<"After 1 left shifting the value is "<<left_shift<<endl;    
+}
